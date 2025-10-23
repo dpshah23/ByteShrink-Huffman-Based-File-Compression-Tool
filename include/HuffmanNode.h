54654f7ee48847f261cbcf6410f7bb295bc23b5d
dpshah23 <131801781+dpshah23@users.noreply.github.com>
2025-10-23 13:40:39 +0530

    Added Huffman Node

--- include/HuffmanNode.h
+++ include/HuffmanNode.h
@@ -1,1 +1,13 @@
-    
+#pragma once
+#include <cstddef>
+
+class HuffmanNode {
+public:
+    unsigned char data;
+    int freq;
+    HuffmanNode* left;
+    HuffmanNode* right;
+
+    HuffmanNode(unsigned char d, int f)
+        : data(d), freq(f), left(nullptr), right(nullptr) {}
+};
